53c9aa6db430f9fa82be253a97b7ba6473cdb6ac
Antoine P <ponvienne.antoine@gmail.com>
2026-01-29 17:19:14 +0100

    question3

--- app/questions/question3.php
+++ app/questions/question3.php
@@ -1,8 +1,11 @@
 <?php
 // ouvrir une session pour crée $_SESSION (toujours zu tout début du code)
-session_start () ;
+session_start();
+
+?>
+<?php
 // On verifie que le formulaire de question 1 a bien été soumis
-if(!empty ($_POST)) {
+if(!empty($_POST)) {
     // Récupére la réponse donnée dans le formulaire
     $answer = $_POST['answer'];
         // Comparé la réponse et j'attrbue le score en fonction de sa validité
@@ -14,7 +17,29 @@ if(!empty ($_POST)) {
         // Stocke la réponse dans la session (attention à bien ouvrir une session avec session_start)
     $_SESSION['score'] = $_SESSION['score'] + $score;
 }
+ $title = 'Question 3';
+require ("../shared/OpenHtml.php");
 
-var_dump($_SESSION);
 
+?>
+       
+<main class="container_Q3">
+    <div class="bloc_image">
+        <h2> Question 3</h2>
+        <img src="../images/dice.jpg" alt="image de cube">
+    </div>
+    <div class="reponses">
+        <p>A quoi ressemblera le cube fini?</p>   
+         <form action="question4.php" method="post">
+            <label for="A"><input type="radio" name="answer" value="A"/>A</label> <br>
+            <label for="B"><input type="radio" name="answer" value="B"/>B</label> <br>
+            <label for="C"><input type="radio" name="answer" value="C"/>C</label> <br>
+            <label for="D"><input type="radio" name="answer" value="D"/>D</label> <br>
+            <input type="submit" value="Valider"> 
+        </form>
+    </div>
+</main>
+
+<?php
+require ("../shared/CloseHtml.php");
 ?>
--- app/questions/question4.php
+++ app/questions/question4.php
@@ -1,11 +1,20 @@
 <?php
-// ouvrir une session pour crée $_SESSION (toujours zu tout début du code)
-
-// On verifie que le formulaire de question 1 a bien été soumis
+session_start();
+?>
+<?php
+// On verifie que le formulaire de question 3 a bien été soumis
+if(!empty($_POST)) {
     // Récupére la réponse donnée dans le formulaire
-    
-    // transformer la fréponse en minuscule pour que DIMANCHE ou Dimanche ou diManche soit valide
-   
+$answer = $_POST['answer'];
     // Comparé la réponse et j'attrbue le score en fonction de sa validité
-    
+    if ($answer === 'C') {
+        $score = 20;
+    } else {
+        $score = 0;
+    }
     // Stocke la réponse dans la session (attention à bien ouvrir une session avec session_start)
+ $_SESSION['score'] = $_SESSION['score'] + $score;
+}
+ var_dump($_SESSION);
+
+ ?>
